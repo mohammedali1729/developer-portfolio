b02fc3f2f459794b4b766ba2631c20be9fa15183
mohammedali1729 <aliasiyashakil@gmail.com>
2025-03-02 20:12:08 +0530

    profolio

---
+++ src/data/headerData.js
@@ -0,0 +1,10 @@
+import resume from '../assets/pdf/Mohammed-ali,BE(Mech)-2023 Full Stack Developer (2) (3) (1).pdf'
+import profileImage from '../assets/images/IMG_20250302_194812.png';// Import the image
+
+export const headerData = {
+    name: 'Mohammed Ali',
+    title: "Back End Developer",
+    desciption: "Experienced Back-End Developer skilled in Python, Flask, Django, and Odoo. Specializes in building scalable, secure applications, database management, API development, and system optimization. Passionate about problem-solving and continuous learning to deliver efficient digital solutions in the ever-evolving tech landscape. ",
+    image:profileImage ,
+    resumePdf: resume
+}
